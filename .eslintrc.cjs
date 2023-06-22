13b2ccc9b02c1654d2588290c7a0b0e1d239b71d
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2023-06-22 23:30:52 +0300

    ci: file structure and configure eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/jsx-runtime',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    'linebreak-style': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    indent: ['error', 2],
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => (
+  <>
+    <h1>Vite + React</h1>
+    <p className="read-the-docs">
+      Click on the Vite and React logos to learn more
+    </p>
+  </>
+);
+
+export default App;
